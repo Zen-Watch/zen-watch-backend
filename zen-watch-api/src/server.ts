ed831355c3d5f205e35b05cacc13645e4c831170
sgdcrypto <sgdheeban@gmail.com>
2023-01-09 03:44:26 -0800

    working ts

--- zen-watch-api/src/server.ts
+++ zen-watch-api/src/server.ts
@@ -1,12 +1,13 @@
-require('dotenv').config()
-const express = require('express')
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express()
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL)
 const db = mongoose.connection
 
-db.on('error', error => console.error(error))
+db.on('error', (error: any) => console.error(error))
 db.once('open', () => console.log('Connected to MongoDB database'))
 
 app.use(express.json())
@@ -14,4 +15,4 @@ app.use(express.json())
 const analyticsRouter = require('./routes/analytics')
 app.use('/analytics', analyticsRouter)
 
-app.listen(3000, ()=> console.log('Server Started!'))
+app.listen(3000, ()=> console.log('Server Started!!'))
