3d5591033560c9e63f2485f26c391509bf050aab
sgdcrypto <sgdheeban@gmail.com>
2023-01-16 15:08:02 -0800

    writing events to mysql

--- zen-watch-api/src/logic/web2/event.logic.ts
+++ zen-watch-api/src/logic/web2/event.logic.ts
@@ -1,12 +1,15 @@
+import { get_developer_by_api_key } from "./developer.logic";
 import { connect_to_mysql } from "../../db/connection_pool";
 
-
-export async function saveEvent(params:any) {
+export async function saveEvent(event:any) {
     try{
-        console.log('INSIDE SAVE EVENT', params);
-        const pool = connect_to_mysql()
-
-
+        const dev = await get_developer_by_api_key(event.api_key);
+        const pool = await connect_to_mysql();
+        const backfill_json = {}
+        const result:any = await pool.query(
+            `insert into event (id, event_type, dev_id, status, event_json, backfill_json) values (?,?,?,?,?,?)`, 
+            [event.event_id, event.event_type, dev.id, "unprocessed", JSON.stringify(event), JSON.stringify(backfill_json)]
+        );
     }catch(e){
         throw e;
     }
--- zen-watch-api/src/utils/constants.ts
+++ zen-watch-api/src/utils/constants.ts
@@ -4,3 +4,4 @@ export const UNPROCESSED_ENTITY = 'unprocessed';
 export const STATUS_OK=200;
 export const STATUS_NOT_FOUND=404;
 export const STATUS_UNPROCESSABLE_ENTITY=422;
+export const INTERNAL_SERVER_ERROR=500;
