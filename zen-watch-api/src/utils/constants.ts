9e0dedb2a972957cf6ff01537decc5bfd7fe4597
sgdcrypto <sgdheeban@gmail.com>
2023-01-16 23:00:32 -0800

    wip

--- zen-watch-api/src/utils/constants.ts
+++ zen-watch-api/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const ONCHAIN_TRANSACTION_EVENT_TYPE = 'onchain-transaction';
+export const ONCHAIN_TRANSACTION_EVENT_TYPE = 'onchain_transaction';
 export const UNPROCESSED_ENTITY = 'unprocessed';
 
 export const STATUS_OK=200;
--- zen-watch-background-job/src/utils/constants.ts
+++ zen-watch-background-job/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const ONCHAIN_TRANSACTION_EVENT_TYPE = 'onchain-transaction';
+export const ONCHAIN_TRANSACTION_EVENT_TYPE = 'onchain_transaction';
 export const UNPROCESSED_ENTITY = 'unprocessed';
