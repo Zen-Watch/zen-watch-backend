517a535c29fadeed8622d5b9f7d15a40dc9a28c5
sgdcrypto <sgdheeban@gmail.com>
2023-01-31 17:36:16 -0800

    fix

--- zen-watch-api/src/db/connection_pool.ts
+++ zen-watch-api/src/db/connection_pool.ts
@@ -15,7 +15,7 @@ export async function connect_to_mysql() {
 
     pool = mysql.createPool({
         host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
+        port: Number(process.env.MYSQL_PORT),
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
