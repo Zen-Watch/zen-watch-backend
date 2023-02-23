58f837ebcb8ec16c9c5f49808c3ce6e2ee279259
sgdcrypto <sgdheeban@gmail.com>
2023-02-23 00:37:50 -0800

    updates to core table

--- zen-watch-admin-api/src/logic/ifttt_action_definition.logic.ts
+++ zen-watch-admin-api/src/logic/ifttt_action_definition.logic.ts
@@ -6,6 +6,9 @@ export async function create_ifttt_action_definition_logic(payload: any) {
     const {
       dev_id,
       categ_id,
+      is_public,
+      is_trusted_source,
+      is_compute_intensive,
       target_resource_name,
       action_name,
       action_description,
@@ -16,7 +19,7 @@ export async function create_ifttt_action_definition_logic(payload: any) {
     } = payload;
 
     // insert payload into ifttt_action_definition table
-    const result: any = await pool!.query(`insert into ifttt_action_definition (dev_id, categ_id, target_resource_name, action_name, action_description, action_signature, action_code, action_expected_output, action_expected_output_description) values (? ,? ,? ,? ,? ,? ,? ,? ,?);`, [dev_id, categ_id, target_resource_name, action_name, action_description, action_signature, action_code, action_expected_output, action_expected_output_description]);    
+    const result: any = await pool!.query(`insert into ifttt_action_definition (dev_id, categ_id, is_public, is_trusted_source, is_compute_intensive, target_resource_name, action_name, action_description, action_signature, action_code, action_expected_output, action_expected_output_description) values (? ,? ,? ,? ,? ,? ,? ,? ,? ,? ,? ,?);`, [dev_id, categ_id, is_public, is_trusted_source, is_compute_intensive, target_resource_name, action_name, action_description, action_signature, action_code, action_expected_output, action_expected_output_description]);
     return result[0];
   } catch (e) {
     throw e;
--- zen-watch-admin-api/src/logic/ifttt_instance.logic.ts
+++ zen-watch-admin-api/src/logic/ifttt_instance.logic.ts
@@ -6,6 +6,10 @@ export async function create_ifttt_instance_logic(payload: any) {
     const {
       ifttt_instance_worker_shard_id,
       dev_id,
+      is_trigger_trusted_source,
+      is_trigger_compute_intensive,
+      is_trigger_push_mechanism,
+      trigger_target_resource_name,
       ifttt_instance_name,
       ifttt_instance_description,
       ifttt_instance_is_on,
@@ -14,7 +18,7 @@ export async function create_ifttt_instance_logic(payload: any) {
     } = payload;
 
     // insert payload into ifttt_instance table
-    const result: any = await pool!.query(`insert into ifttt_instance (ifttt_instance_worker_shard_id, dev_id, ifttt_instance_name, ifttt_instance_description, ifttt_instance_is_on, trigger_info, actions_info) values (?,?,?,?,?,?,?);`, [ifttt_instance_worker_shard_id, dev_id, ifttt_instance_name, ifttt_instance_description, ifttt_instance_is_on, JSON.stringify(trigger_info), JSON.stringify(actions_info)]);
+    const result: any = await pool!.query(`insert into ifttt_instance (ifttt_instance_worker_shard_id, dev_id, is_trigger_trusted_source, is_trigger_compute_intensive, is_trigger_push_mechanism, trigger_target_resource_name, ifttt_instance_name, ifttt_instance_description, ifttt_instance_is_on, trigger_info, actions_info) values (? ,? ,? ,? ,? ,? ,? ,? ,? ,? ,?);`, [ifttt_instance_worker_shard_id, dev_id, is_trigger_trusted_source, is_trigger_compute_intensive, is_trigger_push_mechanism, trigger_target_resource_name, ifttt_instance_name, ifttt_instance_description, ifttt_instance_is_on, JSON.stringify(trigger_info), JSON.stringify(actions_info)]);
     return result[0];
   } catch (e) {
     throw e;
