153632f736359574ea6a31f86b826f7df180ba9f
sgdcrypto <sgdheeban@gmail.com>
2023-02-26 03:17:37 -0800

    wip

--- zen-watch-admin-api/src/routes/ifttt.ts
+++ zen-watch-admin-api/src/routes/ifttt.ts
@@ -18,6 +18,7 @@ router.get('/healthz', (req, res) => {
 // Echo Bot API for IFTTT testing, echos back the request body
 router.post('/test/echo', authenticate_dev_email, (req: Request, res: Response) => {
     res.status(STATUS_OK).send(req.body);
+    //res.status(500).send({status: 500, message: 'test error'});
 })
 
 // ------------------------------- Create APIs -----------------------------------------------
